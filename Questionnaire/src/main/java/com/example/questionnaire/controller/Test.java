b8d7e13c28f599d17bcc7b1eacf949c44560f3ad
Lumbril <32036622+Lumbril@users.noreply.github.com>
2022-03-10 14:59:23 +0300

    get json from request body

--- Questionnaire/src/main/java/com/example/questionnaire/controller/Test.java
+++ Questionnaire/src/main/java/com/example/questionnaire/controller/Test.java
@@ -3,9 +3,13 @@ package com.example.questionnaire.controller;
 import com.example.questionnaire.entity.Role;
 import com.example.questionnaire.service.impl.RoleServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Enumeration;
 
 @Controller
 public class Test {
@@ -30,5 +34,18 @@ public class Test {
         }
 
         return "Successful";
+    }
+
+    @RequestMapping(value = "/json", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public String getRequestAttr(HttpServletRequest request) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        String s;
+
+        while ((s = request.getReader().readLine()) != null) {
+            stringBuilder.append(s);
+        }
+
+        return stringBuilder.toString();
     }
 }
