f26fdbcd47e7c4bbd44f6213dfccff9efcb0cd06
Lumbril <32036622+Lumbril@users.noreply.github.com>
2022-04-22 09:24:36 +0300

    added template giveAnswer

--- Questionnaire/src/main/java/com/example/questionnaire/controller/Question.java
+++ Questionnaire/src/main/java/com/example/questionnaire/controller/Question.java
@@ -3,7 +3,10 @@ package com.example.questionnaire.controller;
 import com.example.questionnaire.dto.QuestionList;
 import com.example.questionnaire.service.impl.QuestionServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +19,10 @@ public class Question {
     @GetMapping("/list")
     public QuestionList getQuestions() {
         return new QuestionList(service.getAll());
+    }
+
+    @PostMapping("/answer")
+    public ResponseEntity giveAnswer() {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Заглушка");
     }
 }
