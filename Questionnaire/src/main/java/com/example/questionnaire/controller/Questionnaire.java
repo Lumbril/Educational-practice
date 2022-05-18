4b9c598ae890c1530a9d47adba233fe589ec1e26
Lumbril <32036622+Lumbril@users.noreply.github.com>
2022-05-18 12:19:53 +0300

    Update Questionnaire.java

--- Questionnaire/src/main/java/com/example/questionnaire/controller/Questionnaire.java
+++ Questionnaire/src/main/java/com/example/questionnaire/controller/Questionnaire.java
@@ -1,5 +1,8 @@
 package com.example.questionnaire.controller;
 
+import com.example.questionnaire.service.impl.UserAnswerServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,8 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/questionnaire")
 public class Questionnaire {
+    @Autowired
+    private UserAnswerServiceImpl userAnswerService;
+    
     @GetMapping("")
     public String getPage() {
+        if (userAnswerService.checkUser(SecurityContextHolder.getContext().getAuthentication().getName())) {
+            return "redirect:/test/user";
+        }
+        
         return "questionnaire";
     }
 }
